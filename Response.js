8d06001bbbd80113d7b2181a68c9f834b163de14
sanchar127 <msan53366@gmail.com>
2023-11-09 10:36:54 +0545

    get data from google api and create the mock data

---
+++ Response.js
@@ -0,0 +1,421 @@
+export default {
+  kind: "customsearch#search",
+  url: {
+    type: "application/json",
+    template:
+      "https://www.googleapis.com/customsearch/v1?q={searchTerms}&num={count?}&start={startIndex?}&lr={language?}&safe={safe?}&cx={cx?}&sort={sort?}&filter={filter?}&gl={gl?}&cr={cr?}&googlehost={googleHost?}&c2coff={disableCnTwTranslation?}&hq={hq?}&hl={hl?}&siteSearch={siteSearch?}&siteSearchFilter={siteSearchFilter?}&exactTerms={exactTerms?}&excludeTerms={excludeTerms?}&linkSite={linkSite?}&orTerms={orTerms?}&relatedSite={relatedSite?}&dateRestrict={dateRestrict?}&lowRange={lowRange?}&highRange={highRange?}&searchType={searchType}&fileType={fileType?}&rights={rights?}&imgSize={imgSize?}&imgType={imgType?}&imgColorType={imgColorType?}&imgDominantColor={imgDominantColor?}&alt=json",
+  },
+  queries: {
+    request: [
+      {
+        title: "Google Custom Search - sanchar",
+        totalResults: "5500000",
+        searchTerms: "sanchar",
+        count: 10,
+        startIndex: 1,
+        inputEncoding: "utf8",
+        outputEncoding: "utf8",
+        safe: "off",
+        cx: "f0fcc45ce7b93438b",
+      },
+    ],
+    nextPage: [
+      {
+        title: "Google Custom Search - sanchar",
+        totalResults: "5500000",
+        searchTerms: "sanchar",
+        count: 10,
+        startIndex: 11,
+        inputEncoding: "utf8",
+        outputEncoding: "utf8",
+        safe: "off",
+        cx: "f0fcc45ce7b93438b",
+      },
+    ],
+  },
+  context: {
+    title: "go-v3",
+  },
+  searchInformation: {
+    searchTime: 0.223205,
+    formattedSearchTime: "0.22",
+    totalResults: "5500000",
+    formattedTotalResults: "5,500,000",
+  },
+  items: [
+    {
+      kind: "customsearch#result",
+      title: "Sanchar Saathi",
+      htmlTitle: "<b>Sanchar</b> Saathi",
+      link: "https://www.sancharsaathi.gov.in/",
+      displayLink: "www.sancharsaathi.gov.in",
+      snippet:
+        "What is Sanchar Saathi? Sanchar Saathi portal is a citizen centric initiative of Department of Telecommunications to empower mobile subscribers, strengthen ...",
+      htmlSnippet:
+        "What is <b>Sanchar</b> Saathi? <b>Sanchar</b> Saathi portal is a citizen centric initiative of Department of Telecommunications to empower mobile subscribers, strengthen&nbsp;...",
+      formattedUrl: "https://www.sancharsaathi.gov.in/",
+      htmlFormattedUrl: "https://www.<b>sanchar</b>saathi.gov.in/",
+      pagemap: {
+        metatags: [
+          {
+            viewport: "width=device-width, initial-scale=1",
+          },
+        ],
+      },
+    },
+    {
+      kind: "customsearch#result",
+      title: "Sanchar e-Paper Main Edition",
+      htmlTitle: "<b>Sanchar</b> e-Paper Main Edition",
+      link: "http://www.esanchar.co.in/",
+      displayLink: "www.esanchar.co.in",
+      snippet: "Sanchar e-Paper Main Edition.",
+      htmlSnippet: "<b>Sanchar</b> e-Paper Main Edition.",
+      cacheId: "YYo8HfeFzJcJ",
+      formattedUrl: "www.esanchar.co.in/",
+      htmlFormattedUrl: "www.e<b>sanchar</b>.co.in/",
+      pagemap: {
+        cse_thumbnail: [
+          {
+            src: "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQv_X347-JzjnzvYDzl-4oxdivq-jdKvEbw_SSxJ1RN3rUTueT256KfdP3_",
+            width: "175",
+            height: "288",
+          },
+        ],
+        metatags: [
+          {
+            "fb:app_id": "236448743096783",
+            viewport: "width=device-width, initial-scale=1",
+            news_keywords: "Sanchar e-Paper Main Edition",
+          },
+        ],
+        cse_image: [
+          {
+            src: "http://www.esanchar.co.in/epaperimages//09112023//09112023-md-hr-1.jpg",
+          },
+        ],
+      },
+    },
+    {
+      kind: "customsearch#result",
+      title: "TAFCOP",
+      htmlTitle: "TAFCOP",
+      link: "https://tafcop.sancharsaathi.gov.in/",
+      displayLink: "tafcop.sancharsaathi.gov.in",
+      snippet:
+        "Sanchar Saathi · About · Citizen Centric Services · Keep Yourself Aware · FAQs · In Social Media · Image Gallery · Useful Links · Authorized Login.",
+      htmlSnippet:
+        "<b>Sanchar</b> Saathi &middot; About &middot; Citizen Centric Services &middot; Keep Yourself Aware &middot; FAQs &middot; In Social Media &middot; Image Gallery &middot; Useful Links &middot; Authorized Login.",
+      cacheId: "kyKt3UfJ388J",
+      formattedUrl: "https://tafcop.sancharsaathi.gov.in/",
+      htmlFormattedUrl: "https://tafcop.<b>sanchar</b>saathi.gov.in/",
+      pagemap: {
+        cse_thumbnail: [
+          {
+            src: "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRpGXvP6M5vx2FUE-ksHYzylpdG9TZRcPfGzkIbI-yXyWHzmvWsuNvJCH2I",
+            width: "128",
+            height: "128",
+          },
+        ],
+        metatags: [
+          {
+            viewport: "width=device-width, initial-scale=1, shrink-to-fit=no",
+            author: "C-DOT",
+            _csrf_header: "X-CSRF-TOKEN",
+            _csrf: "69c3c344-8680-430f-9262-e516ae21599e",
+            _csrf_parameter: "_csrf",
+          },
+        ],
+        cse_image: [
+          {
+            src: "https://tafcop.sancharsaathi.gov.in/telecomUser/resources/img/tafcop-citizen-centric.png",
+          },
+        ],
+      },
+    },
+    {
+      kind: "customsearch#result",
+      title: "CEIR",
+      htmlTitle: "CEIR",
+      link: "https://ceir.sancharsaathi.gov.in/",
+      displayLink: "ceir.sancharsaathi.gov.in",
+      snippet:
+        "Sanchar Saathi · India GOV · India G20 · Azadi Mahotsav · Swach Bharat · Go to home ... Sanchar Saathi · Block Stolen/Lost Mobile · Un-Block Found Mobile · Check ...",
+      htmlSnippet:
+        "<b>Sanchar</b> Saathi &middot; India GOV &middot; India G20 &middot; Azadi Mahotsav &middot; Swach Bharat &middot; Go to home ... <b>Sanchar</b> Saathi &middot; Block Stolen/Lost Mobile &middot; Un-Block Found Mobile &middot; Check&nbsp;...",
+      cacheId: "sVUzIeaH4WMJ",
+      formattedUrl: "https://ceir.sancharsaathi.gov.in/",
+      htmlFormattedUrl: "https://ceir.<b>sanchar</b>saathi.gov.in/",
+      pagemap: {
+        metatags: [
+          {
+            viewport: "width=device-width, initial-scale=1.0",
+          },
+        ],
+      },
+    },
+    {
+      kind: "customsearch#result",
+      title:
+        "Black Salt (Iodised salt / kala namak / sanchar) - 200g ... - Amazon.com",
+      htmlTitle:
+        "Black Salt (Iodised salt / kala namak / sanchar) - 200g ... - Amazon.com",
+      link: "https://www.amazon.com/Black-Salt-Iodised-namak-sanchar/dp/B00PWN12DY",
+      displayLink: "www.amazon.com",
+      snippet:
+        "Amazon.com : Black Salt (Iodised salt / kala namak / sanchar) - 200g : Grocery & Gourmet Food.",
+      htmlSnippet:
+        "Amazon.com : Black Salt (Iodised salt / kala namak / <b>sanchar</b>) - 200g : Grocery &amp; Gourmet Food.",
+      cacheId: "-SPqa7HdNsYJ",
+      formattedUrl:
+        "https://www.amazon.com/Black-Salt-Iodised...sanchar/dp/B00PWN12DY",
+      htmlFormattedUrl:
+        "https://www.amazon.com/Black-Salt-Iodised...<b>sanchar</b>/dp/B00PWN12DY",
+      pagemap: {
+        scraped: [
+          {
+            image_link:
+              "https://m.media-amazon.com/images/I/51n-BSyX9ML._AC_UF1000,1000_QL80_.jpg",
+          },
+        ],
+        cse_thumbnail: [
+          {
+            src: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRstnaH3dzL6yyuYsD2oRF0WbiLSBBpBb9EUM3PbUr5EQaO7NoOd2S7P7o",
+            width: "233",
+            height: "216",
+          },
+        ],
+        metatags: [
+          {
+            "og:image":
+              "https://m.media-amazon.com/images/I/51n-BSyX9ML._SR600%2c315_PIWhiteStrip%2cBottomLeft%2c0%2c35_PIStarRatingFOURANDHALF%2cBottomLeft%2c360%2c-6_SR600%2c315_ZA66%2c445%2c290%2c400%2c400%2cAmazonEmberBold%2c12%2c4%2c0%2c0%2c5_SCLZZZZZZZ_FMpng_BG255%2c255%2c255.jpg",
+            "theme-color": "#131921",
+            viewport:
+              "width=device-width, maximum-scale=1, minimum-scale=1, initial-scale=1, user-scalable=no, shrink-to-fit=no",
+            "og:title":
+              "Black Salt (Iodised salt / kala namak / sanchar) - 200g",
+            "og:url": "https://www.amazon.com/dp/B00PWN12DY/ref=tsm_1_fb_lk",
+            title:
+              "Amazon.com : Black Salt (Iodised salt / kala namak / sanchar) - 200g : Grocery & Gourmet Food",
+            "og:description":
+              "Black Salt (Light Pinkish Purple In Colour). Has Many Names To It. This Is An Authentic Spice Which Is Used In Many Dishes. Used To Give Food A Bit Of A Different Taste To Normal Salt.",
+          },
+        ],
+        cse_image: [
+          {
+            src: "https://m.media-amazon.com/images/I/51n-BSyX9ML._AC_UF1000,1000_QL80_.jpg",
+          },
+        ],
+      },
+    },
+    {
+      kind: "customsearch#result",
+      title: "CEIR IMEI Verification",
+      htmlTitle: "CEIR IMEI Verification",
+      link: "https://ceir.sancharsaathi.gov.in/Device/CeirImeiVerification.jsp",
+      displayLink: "ceir.sancharsaathi.gov.in",
+      snippet:
+        "Sanchar Saathi. India GOV. India G20. Azadi Mahotsav. Swach Bharat. Sanchar Saathi · India GOV · India G20 · Azadi Mahotsav · Swach Bharat · Go to home. CEIR ...",
+      htmlSnippet:
+        "<b>Sanchar</b> Saathi. India GOV. India G20. Azadi Mahotsav. Swach Bharat. <b>Sanchar</b> Saathi &middot; India GOV &middot; India G20 &middot; Azadi Mahotsav &middot; Swach Bharat &middot; Go to home. CEIR&nbsp;...",
+      cacheId: "ZWRsGHV8E2UJ",
+      formattedUrl:
+        "https://ceir.sancharsaathi.gov.in/Device/CeirImeiVerification.jsp",
+      htmlFormattedUrl:
+        "https://ceir.<b>sanchar</b>saathi.gov.in/Device/CeirImeiVerification.jsp",
+      pagemap: {
+        metatags: [
+          {
+            viewport: "width=device-width, initial-scale=1.0",
+          },
+        ],
+      },
+    },
+    {
+      kind: "customsearch#result",
+      title: "Sanchar Saathi Mobile Tracker - Apps on Google Play",
+      htmlTitle: "<b>Sanchar</b> Saathi Mobile Tracker - Apps on Google Play",
+      link: "https://play.google.com/store/apps/details?id=com.sancharsaathi.portal&hl=en_US",
+      displayLink: "play.google.com",
+      snippet:
+        "Jun 14, 2023 ... Sanchar Saathi empowers citizens by allowing them to know the mobile connections issued in their name, get disconnected the connections not ...",
+      htmlSnippet:
+        "Jun 14, 2023 <b>...</b> <b>Sanchar</b> Saathi empowers citizens by allowing them to know the mobile connections issued in their name, get disconnected the connections not&nbsp;...",
+      cacheId: "KgiGrg2m-QcJ",
+      formattedUrl:
+        "https://play.google.com/store/apps/details?id=com.sancharsaathi.portal...",
+      htmlFormattedUrl:
+        "https://play.google.com/store/apps/details?id=com.<b>sanchar</b>saathi.portal...",
+      pagemap: {
+        offer: [
+          {
+            price: "0",
+            url: "https://play.google.com/store/apps/details?id=com.sancharsaathi.portal&rdid=com.sancharsaathi.portal&feature=md&offerId",
+          },
+        ],
+        cse_thumbnail: [
+          {
+            src: "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQGZGW9cSRHpVEX2U3z6RfvC3Yn7uTnXdVTKfukRrlxSFajMvoDAhRED5s",
+            width: "225",
+            height: "225",
+          },
+        ],
+        metatags: [
+          {
+            "og:image":
+              "https://play-lh.googleusercontent.com/_cdMlQmNczhJaCP-k6Ivfx4__pq8T9UYQkgsB62mRk2-EushR8BqdxfVkB7mxWgUNw",
+            "og:type": "website",
+            "twitter:card": "summary_large_image",
+            "twitter:title":
+              "Sanchar Saathi Mobile Tracker - Apps on Google Play",
+            "twitter:url":
+              "https://play.google.com/store/apps/details?id=com.sancharsaathi.portal&hl=en_US",
+            "appstore:developer_url": "https://tekdost.com",
+            "og:title": "Sanchar Saathi Mobile Tracker - Apps on Google Play",
+            "twitter:image":
+              "https://play-lh.googleusercontent.com/_cdMlQmNczhJaCP-k6Ivfx4__pq8T9UYQkgsB62mRk2-EushR8BqdxfVkB7mxWgUNw=w600-h300-pc0xffffff-pd",
+            "appstore:bundle_id": "com.sancharsaathi.portal",
+            referrer: "origin",
+            "twitter:site": "@GooglePlay",
+            "appstore:store_id": "com.sancharsaathi.portal",
+            viewport: "width=device-width, initial-scale=1",
+            "apple-mobile-web-app-capable": "yes",
+            "twitter:description":
+              "Find or Track Lost Mobile Phones And Sim Cards Sanchar Saathi",
+            "mobile-web-app-capable": "yes",
+            "og:url":
+              "https://play.google.com/store/apps/details?id=com.sancharsaathi.portal&hl=en_US",
+          },
+        ],
+        cse_image: [
+          {
+            src: "https://play-lh.googleusercontent.com/_cdMlQmNczhJaCP-k6Ivfx4__pq8T9UYQkgsB62mRk2-EushR8BqdxfVkB7mxWgUNw",
+          },
+        ],
+      },
+    },
+    {
+      kind: "customsearch#result",
+      title: "SECTOR 4, PLOT 15, SANCHAR VIHAR APARTMENT, DWARKA ...",
+      htmlTitle:
+        "SECTOR 4, PLOT 15, <b>SANCHAR</b> VIHAR APARTMENT, DWARKA ...",
+      link: "https://www.google.com/mymaps/viewer?mid=17J0xLYKXM9FdZlC5YARJgkVzVS0&hl=en",
+      displayLink: "www.google.com",
+      snippet:
+        "Oct 7, 2023 ... Any Real Estate query please call or click +91 999999 3941 https://www.dwarkaapartment.com/",
+      htmlSnippet:
+        "Oct 7, 2023 <b>...</b> Any Real Estate query please call or click +91 999999 3941 https://www.dwarkaapartment.com/",
+      cacheId: "CFW5l0TedW8J",
+      formattedUrl: "https://www.google.com/mymaps/viewer?mid...hl=en",
+      htmlFormattedUrl: "https://www.google.com/mymaps/viewer?mid...hl=en",
+      pagemap: {
+        cse_thumbnail: [
+          {
+            src: "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcSQ7DX6VbMD3T1H-XkmqiaMR-AQMrDh2F6IkXceKtpjYT5rCVAiy-HMNARo",
+            width: "221",
+            height: "228",
+          },
+        ],
+        website: [
+          {
+            image:
+              "https://www.google.com/maps/d/thumbnail?mid=17J0xLYKXM9FdZlC5YARJgkVzVS0&hl=en",
+            name: "SECTOR 4, PLOT 15, SANCHAR VIHAR APARTMENT, DWARKA - Google My Maps",
+            description:
+              "Any Real Estate query please call or click +91 999999 3941 https://www.dwarkaapartment.com/",
+            url: "https://www.google.com/maps/d/viewer?mid=17J0xLYKXM9FdZlC5YARJgkVzVS0&hl=en",
+          },
+        ],
+        metatags: [
+          {
+            "og:image":
+              "https://www.google.com/maps/d/thumbnail?mid=17J0xLYKXM9FdZlC5YARJgkVzVS0&hl=en",
+            "apple-itunes-app":
+              "app-id=585027354, app-argument=comgooglemaps://?mapsurl=https://www.google.com/mymaps/viewer?mid%3D17J0xLYKXM9FdZlC5YARJgkVzVS0%26hl%3Den",
+            "og:type": "website",
+            "twitter:card": "summary_large_image",
+            "twitter:title":
+              "SECTOR 4, PLOT 15, SANCHAR VIHAR APARTMENT, DWARKA - Google My Maps",
+            "og:site_name": "Google My Maps",
+            viewport:
+              "initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=0,width=device-width",
+            "twitter:description":
+              "Any Real Estate query please call or click\n+91 999999 3941\nhttps://www.dwarkaapartment.com/",
+            "og:title":
+              "SECTOR 4, PLOT 15, SANCHAR VIHAR APARTMENT, DWARKA - Google My Maps",
+            "og:url":
+              "https://www.google.com/maps/d/viewer?mid=17J0xLYKXM9FdZlC5YARJgkVzVS0&hl=en",
+            "og:description":
+              "Any Real Estate query please call or click\n+91 999999 3941\nhttps://www.dwarkaapartment.com/",
+            "twitter:image:src":
+              "https://www.google.com/maps/d/thumbnail?mid=17J0xLYKXM9FdZlC5YARJgkVzVS0&hl=en",
+          },
+        ],
+        cse_image: [
+          {
+            src: "https://www.google.com/maps/d/thumbnail?mid=17J0xLYKXM9FdZlC5YARJgkVzVS0&hl=en",
+          },
+        ],
+      },
+    },
+    {
+      kind: "customsearch#result",
+      title: "Project SANCHAR",
+      htmlTitle: "Project <b>SANCHAR</b>",
+      link: "https://projectsanchar.org/",
+      displayLink: "projectsanchar.org",
+      snippet:
+        "What is SANCHAR? SANCHAR is an effort to streamline the search of health journalists by providing data and evidence from scientifically validated sources, and ...",
+      htmlSnippet:
+        "What is <b>SANCHAR</b>? <b>SANCHAR</b> is an effort to streamline the search of health journalists by providing data and evidence from scientifically validated sources, and&nbsp;...",
+      cacheId: "wcxlySEv9Y0J",
+      formattedUrl: "https://projectsanchar.org/",
+      htmlFormattedUrl: "https://project<b>sanchar</b>.org/",
+      pagemap: {
+        metatags: [
+          {
+            viewport: "width=device-width, initial-scale=1",
+          },
+        ],
+      },
+    },
+    {
+      kind: "customsearch#result",
+      title: "Sanchar Saathi Portal| National Portal of India",
+      htmlTitle: "<b>Sanchar</b> Saathi Portal| National Portal of India",
+      link: "https://www.india.gov.in/spotlight/sanchar-saathi-portal",
+      displayLink: "www.india.gov.in",
+      snippet:
+        "Jun 26, 2023 ... National Portal of India provides a single-window access to information and services that are electronically delivered from all Government ...",
+      htmlSnippet:
+        "Jun 26, 2023 <b>...</b> National Portal of India provides a single-window access to information and services that are electronically delivered from all Government&nbsp;...",
+      cacheId: "P-qF7HalSGUJ",
+      formattedUrl: "https://www.india.gov.in/spotlight/sanchar-saathi-portal",
+      htmlFormattedUrl:
+        "https://www.india.gov.in/spotlight/<b>sanchar</b>-saathi-portal",
+      pagemap: {
+        cse_thumbnail: [
+          {
+            src: "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTopHSUBmNZ7CabkU5zun9hZFGGRzLjn94IhJO90YCLImlUbuGvipWfJ8o",
+            width: "225",
+            height: "225",
+          },
+        ],
+        metatags: [
+          {
+            handheldfriendly: "true",
+            viewport: "width=device-width, initial-scale=1",
+            mobileoptimized: "width",
+            title: "National Portal of India",
+            lang: "English",
+          },
+        ],
+        cse_image: [
+          {
+            src: "https://www.india.gov.in/sites/upload_files/npi/files/newsletter/logo_share.png",
+          },
+        ],
+      },
+    },
+  ],
+};
