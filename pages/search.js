fb09d1ec6bdc43d7cce3b1a97b45921e81803a77
sanchar127 <msan53366@gmail.com>
2023-11-10 09:50:54 +0545

    correcting mistake

--- pages/search.js
+++ pages/search.js
@@ -1,20 +1,18 @@
-import Response from "@/Response";
-import SearchHeader from "@/components/SearchHeader";
 import Head from "next/head";
+import SearchHeader from "@/components/SearchHeader";
 import SearchResults from "@/components/SearchResults";
-import { useRouter } from "next/router";
 import ImageResults from "@/components/ImageResults";
-const search = ({ results }) => {
-  console.log(results);
+import { useRouter } from "next/router";
+
+const Search = ({ results }) => {
   const router = useRouter();
+
   return (
     <div>
       <Head>
-        <title>{router.query.term}- Search page</title>
+        <title>{router.query.term} - Search page</title>
       </Head>
-      {/* search Header */}
       <SearchHeader />
-      {/*search web and images Results*/}
       {router.query.searchType === "image" ? (
         <ImageResults results={results} />
       ) : (
@@ -24,23 +22,36 @@ const search = ({ results }) => {
   );
 };
 
-export default search;
+export default Search;
 
 export async function getServerSideProps(context) {
   const startIndex = context.query.start || "1";
   const mockData = false;
-  const data = mockData
-    ? Response
-    : await fetch(
-        `https://www.googleapis.com/customsearch/v1?key=${
-          process.env.API_KEY
-        }&cx=${process.env.CONTEXT_KEY}&q=${context.query.term}${
-          context.query.searchType && "&searchType=image"
-        }&start=${startIndex}`
-      ).then((response) => response.json());
+
+  const searchTypeParam = context.query.searchType
+    ? `&searchType=${context.query.searchType}`
+    : "";
+  const apiUrl = `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${context.query.term}${searchTypeParam}&start=${startIndex}`;
+
+  let data = null;
+
+  try {
+    const response = await fetch(apiUrl);
+
+    if (response.ok) {
+      data = await response.json();
+    } else {
+      // Handle the error case here, e.g., log the error or return an error response.
+      console.error(`Failed to fetch data. Status: ${response.status}`);
+    }
+  } catch (error) {
+    // Handle network errors here.
+    console.error(`Network error: ${error.message}`);
+  }
+
   return {
     props: {
-      results: data,
+      results: data || null,
     },
   };
 }
