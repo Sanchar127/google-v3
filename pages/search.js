86fb94d0c2197a21f1eeccca63ff9f6feb87bdee
sanchar127 <msan53366@gmail.com>
2023-11-10 09:55:24 +0545

    fix the searchjs

--- pages/search.js
+++ pages/search.js
@@ -1,18 +1,20 @@
-import Head from "next/head";
+import Response from "@/Response";
 import SearchHeader from "@/components/SearchHeader";
+import Head from "next/head";
 import SearchResults from "@/components/SearchResults";
-import ImageResults from "@/components/ImageResults";
 import { useRouter } from "next/router";
-
+import ImageResults from "@/components/ImageResults";
 const Search = ({ results }) => {
+  console.log(results);
   const router = useRouter();
-
   return (
     <div>
       <Head>
-        <title>{router.query.term} - Search page</title>
+        <title>{router.query.term}- Search page</title>
       </Head>
+      {/* search Header */}
       <SearchHeader />
+      {/*search web and images Results*/}
       {router.query.searchType === "image" ? (
         <ImageResults results={results} />
       ) : (
@@ -26,32 +28,19 @@ export default Search;
 
 export async function getServerSideProps(context) {
   const startIndex = context.query.start || "1";
-  const mockData = false;
-
-  const searchTypeParam = context.query.searchType
-    ? `&searchType=${context.query.searchType}`
-    : "";
-  const apiUrl = `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${context.query.term}${searchTypeParam}&start=${startIndex}`;
-
-  let data = null;
-
-  try {
-    const response = await fetch(apiUrl);
-
-    if (response.ok) {
-      data = await response.json();
-    } else {
-      // Handle the error case here, e.g., log the error or return an error response.
-      console.error(`Failed to fetch data. Status: ${response.status}`);
-    }
-  } catch (error) {
-    // Handle network errors here.
-    console.error(`Network error: ${error.message}`);
-  }
-
+  const mockData = true;
+  const data = mockData
+    ? Response
+    : await fetch(
+        `https://www.googleapis.com/customsearch/v1?key=${
+          process.env.API_KEY
+        }&cx=${process.env.CONTEXT_KEY}&q=${context.query.term}${
+          context.query.searchType && "&searchType=image"
+        }&start=${startIndex}`
+      ).then((response) => response.json());
   return {
     props: {
-      results: data || null,
+      results: data,
     },
   };
 }
